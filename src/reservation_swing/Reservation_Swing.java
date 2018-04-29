3e2ea054bed37511e259324be31a8249d02c06a1
tux18 <36100434+tux18@user.noreply.github.com>
2018-04-30 07:41:44 +0800

    Initial Commit

---
+++ src/reservation_swing/Reservation_Swing.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reservation_swing;
+
+
+public class Reservation_Swing {
+
+
+    public static void main(String[] args) {
+       Main call = new Main();
+       call.show();
+    }
+    
+}
